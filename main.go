106affa01ce1f478c12e1c1c8d7d9c9eb4917c52
Shreedeep Datta <shreedeepdatta2@gmail.com>
2025-02-24 22:15:46 +0530

    Implemented the parseTkhdBox function

--- main.go
+++ main.go
@@ -11,12 +11,31 @@ type Box struct {
 	Size uint32
 	Type [4]byte
 }
+type Tkhbd struct {
+	Version  uint8
+	Flags    [3]byte
+	TrackID  uint32
+	Duration uint32
+	Width    uint32
+	Height   uint32
+}
 
 func parseTkhdBox(file *os.File, size uint32) {
 	slog.Info("Parsing tkhd box", "size", size)
+	var tkhd Tkhbd
+	binary.Read(file, binary.BigEndian, &tkhd.Version)
+	binary.Read(file, binary.BigEndian, &tkhd.Flags)
+	binary.Read(file, binary.BigEndian, &tkhd.TrackID)
+	file.Seek(4, 1)
+	binary.Read(file, binary.BigEndian, &tkhd.Duration)
+	file.Seek(40, 1)
+	binary.Read(file, binary.BigEndian, &tkhd.Width)
+	binary.Read(file, binary.BigEndian, &tkhd.Height)
+	slog.Info("Track info", "Track ID", tkhd.TrackID, "Duration", tkhd.Duration, "Width", tkhd.Width, "height", tkhd.Height)
 }
 func parseMdiaBox(file *os.File, size uint32) {
 	slog.Info("Parsing mdia box", "size", size)
+
 }
 func parseTrakBox(file *os.File, size uint32) {
 	slog.Info("Parsing trak box", "size", size)
